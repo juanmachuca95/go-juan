9b60c20173bf3c8a7918f5031ae1c6dce9ae1a9c
Juan <machucajuangabriel@gmail.com>
2021-04-18 13:50:21 -0300

    Test to local

--- internal/database/mysql_connection.go
+++ internal/database/mysql_connection.go
@@ -13,23 +13,27 @@ var db *sql.DB
 // Connect function
 func Connect(db *sql.DB) *sql.DB {
 
-	if os.Getenv("DEFAULT_CONNECTION") == true {
+	var host string 
+	var user string 
+	var password string 
+	var dbname string 
+
+	if os.Getenv("DEFAULT_CONNECTION") == "true" {
 		fmt.Println("Utilizar la connection en local")
 		// Database settings
-		var (
-			host     = os.Getenv("HOST_LOCAL")
-			user     = os.Getenv("USER_LOCAL")
-			password = os.Getenv("PASSWORD_LOCAL")
-			dbname   = os.Getenv("DATABASE_LOCAL")
-		)
+		host     = os.Getenv("HOST_LOCAL")
+		user     = os.Getenv("USER_LOCAL")
+		password = os.Getenv("PASSWORD_LOCAL")
+		dbname   = os.Getenv("DATABASE_LOCAL")
+		
 	}else {
 		// Database settings
-		var (
-			host     = os.Getenv("HOST")
-			user     = os.Getenv("USER")
-			password = os.Getenv("PASSWORD")
-			dbname   = os.Getenv("DATABASE")
-		)
+		
+		host     = os.Getenv("HOST")
+		user     = os.Getenv("USER")
+		password = os.Getenv("PASSWORD")
+		dbname   = os.Getenv("DATABASE")
+	
 		fmt.Println("Utilizar la conección de producción")
 	}
 	
