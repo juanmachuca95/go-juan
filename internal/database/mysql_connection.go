badf674f1c324ac6740de0a03047cf567a96f9d9
Juan <machucajuangabriel@gmail.com>
2021-04-18 13:42:03 -0300

    Swich of connections to database

--- internal/database/mysql_connection.go
+++ internal/database/mysql_connection.go
@@ -13,15 +13,34 @@ var db *sql.DB
 // Connect function
 func Connect(db *sql.DB) *sql.DB {
 
-	// Database settings
-	var (
-		host     = os.Getenv("HOST")
-		user     = os.Getenv("USER")
-		password = os.Getenv("PASSWORD")
-		dbname   = os.Getenv("DATABASE")
-	)
+	if os.Getenv("DEFAULT_CONNECTION") == true {
+		fmt.Println("Utilizar la connection en local")
+		// Database settings
+		var (
+			host     = os.Getenv("HOST_LOCAL")
+			user     = os.Getenv("USER_LOCAL")
+			password = os.Getenv("PASSWORD_LOCAL")
+			dbname   = os.Getenv("DATABASE_LOCAL")
+		)
+	}else {
+		// Database settings
+		var (
+			host     = os.Getenv("HOST")
+			user     = os.Getenv("USER")
+			password = os.Getenv("PASSWORD")
+			dbname   = os.Getenv("DATABASE")
+		)
+		fmt.Println("Utilizar la conección de producción")
+	}
+	
 
 	fmt.Printf("%s", host)
+	fmt.Println("***********")
+	fmt.Printf("%s", user)
+	fmt.Println("***********")
+	fmt.Printf("%s", password)
+	fmt.Println("***********")
+	fmt.Printf("%s", dbname)
 
 	var err error
 	// Use DSN string to open
