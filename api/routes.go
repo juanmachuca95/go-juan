53de8fdc3c359010325682b7609414f7f6725749
Juan <machucajuangabriel@gmail.com>
2021-04-22 15:55:46 -0300

    Test production

--- api/routes.go
+++ api/routes.go
@@ -6,6 +6,10 @@ import "github.com/gofiber/fiber/v2"
 /*Mis routes*/
 func SetupAppRoutes(app *fiber.App) {
 
+	app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hola 👋! Bienvenido a mi API Golang")
+    })
+
 	app.Post("/login", login)
 
 	app.Use(authRequired()).Get("/padron", getPadron) // READ PADRON
@@ -14,10 +18,7 @@ func SetupAppRoutes(app *fiber.App) {
 
 	app.Use(authRequired()).Post("/updatepadron", updatePadron) // UPDATE PADRON
 
-	//app.Get("")
-	app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hola 👋! Bienvenido a mi API Golang")
-    })
+
 
 }
 
