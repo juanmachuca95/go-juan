d61c04491a8a680ea4e7fae66a929e34050e7aca
Machuca <machucajuangabriel@gmail.com>
2021-04-28 15:00:44 -0300

    Bool voto

--- api/services.go
+++ api/services.go
@@ -87,7 +87,8 @@ func storeDniPadron(c *fiber.Ctx) error {
 		})
 	}
 
-	res, err := db.Query( InsertDniPadron(), p.Dni, "1")
+	bool voto = true;
+	res, err := db.Query( InsertDniPadron(), p.Dni, voto)
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error":"Dni duplicado o con valores incorrectos.",
